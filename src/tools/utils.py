db8a5a9e47a746dcd23dacb7a662710a1c2fffa9
antth <antth@hotmail.fi>
2019-11-07 10:34:34 +0200

    file reading rule

--- src/tools/utils.py
+++ src/tools/utils.py
@@ -3,17 +3,17 @@ from pathlib import Path
 
 def text_file_generator(
         data: str,
-        file_extension: str = 'txt',
+        rule: str,
 ):
     fp = Path(data)
     if not fp.exists():
         raise ValueError(f"Specified data path {str(fp)} does not exist")
-    for file in fp.rglob(f'*.{file_extension}'):
+    for file in fp.rglob(rule):
         text = file.read_text()
         yield text
 
 
 if __name__ == '__main__':
     DATA = '../../data/raw'
-    texts = sorted(text_file_generator(DATA))
+    texts = sorted(text_file_generator(DATA, 'roa_1809*.txt'))
     print(len(texts))
