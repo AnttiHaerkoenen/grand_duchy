bafc98bbcb2ac7535834467ecb1395b508a1163e
antth <antth@hotmail.fi>
2019-11-12 12:36:31 +0200

    freq plotting

--- src/visualization/plot_frequency.py
+++ src/visualization/plot_frequency.py
@@ -8,4 +8,4 @@ import bokeh
 if __name__ == '__main__':
     data = pd.read_csv('../../data/processed/frequencies_riksdag_all.csv')
     data.plot(x='year', title="Frequency / 100 000 words")
-    plt.savefig(fname='storfurste_freq')
+    plt.savefig(fname='../../reports/figures/storfurste_freq')
