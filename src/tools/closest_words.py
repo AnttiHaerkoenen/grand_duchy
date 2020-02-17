4aa4b2e0cf323c73ff4267715a5f83748befa886
antth <antth@hotmail.fi>
2020-02-17 14:00:14 +0200

    closest_words.py

--- src/tools/closest_words.py
+++ src/tools/closest_words.py
@@ -6,16 +6,65 @@ import gensim
 
 def get_closest_words(
         *,
-        models: list,
+        models: dict,
         words: list,
-        min_similarity: int = None,
+        min_similarity: float = 0,
         max_n: int = 10,
-):
-    # TODO
-    closest = model.wv.most_similar(*words, topn=max_n)
+) -> dict:
+    results = {w: {} for w in words}
 
-    return result
+    for name, model in models.items():
+        for w in words:
+            if w in model.wv:
+                closest = model.wv.most_similar(w, topn=max_n)
+                results[w][name] = [t for t in closest if t[1] >= min_similarity]
+
+    return results
+
+
+def make_closest_words_table(
+        word_data: dict,
+) -> pd.DataFrame:
+
+    df = pd.DataFrame.from_dict(word_data)
+    df = df.applymap(lambda e: e[0])
+
+    return df
+
+
+def make_close_words_lists(
+        input_file: Path,
+        input_column: str,
+        output_dir: Path,
+        models: dict,
+) -> None:
+    words = pd.read_csv(str(input_file))[input_column]
+    words = [w.casefold() for w in words]
+
+    closest_words = get_closest_words(
+        models=models,
+        words=words,
+    )
+
+    for w, d in closest_words.items():
+        table = make_closest_words_table(d)
+        table.to_csv(str(output_dir / f'closest_{w}.csv'))
 
 
 if __name__ == '__main__':
     model_dir = Path('../../models/SGNS_UPDATE')
+
+    years = list(range(1740, 1901, 20))
+
+    models = {
+        f'sv_{year}': gensim.models.Word2Vec.load(str(model_dir / 'sv' / f'sv_{year}.w2v'))
+        for year
+        in years
+    }
+
+    make_close_words_lists(
+        input_file=Path('../../wordlists/seed_words.csv'),
+        input_column='Swedish',
+        output_dir=Path('../../wordlists/sv_close_words'),
+        models=models,
+    )
