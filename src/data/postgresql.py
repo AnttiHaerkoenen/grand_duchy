7a1a6201065300fa0faa8b1ce58bd142b37bbb48
AnttiHaerkoenen <antth@uef.fi>
2021-02-25 15:00:28 +0200

    retrying

--- src/data/postgresql.py
+++ src/data/postgresql.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Sequence
+import time
 
 import pandas as pd
 from sqlalchemy import create_engine
@@ -7,6 +8,34 @@ from sqlalchemy.engine import Engine
 from sqlalchemy.exc import ProgrammingError, OperationalError
 
 
+class DataBaseError(Exception):
+    pass
+
+
+def retry(retries=10, timeout=5):
+    def wraps(f):
+        def inner(*args, **kwargs):
+            for i in range(retries):
+                if i > 0:
+                    print(f'Retrying, attempt {i}')
+                try:
+                    result = f(*args, **kwargs)
+                except (ProgrammingError, OperationalError) as e:
+                    print(f'Something went wrong: {e}')
+                    time.sleep(timeout + i * 10)
+                    continue
+                except Exception as e:
+                    print(f'Unknown error: {e}')
+                    continue
+                else:
+                    return result
+            else:
+                raise DataBaseError("Database operation failed")
+        return inner
+    return wraps
+
+
+@retry(5, 1000)
 def populate_database(
         *,
         data_dir: Path,
@@ -24,57 +53,56 @@ def populate_database(
         )
 
         for file in sorted(path.iterdir()):
-            print(f'Reading {file.name}')
-
-            df = pd.read_csv(file)
-
-            if df.empty:
-                print(f'{file.name} is empty, moving on')
-                continue
-
-            df.sort_values(by='year', inplace=True)
-
-            df = df.head(size_limit)
-            df.index.rename('index', inplace=False)
-
-            df['term'] = [file.stem] * len(df.index)
-
-            if 'type' in df.columns:
-                df['lemma'] = df.type.isin(['lemma', 'both'])
-                df['regex'] = df.type.isin(['regex', 'both'])
-                df.drop(columns='type', inplace=True)
-
-            print(f'Saving {file.stem} to database')
-
-            try:
-                df.to_sql(
-                    directory,
-                    con=engine,
-                    if_exists='append',
-                    index=False,
-                    chunksize=100,
-                )
-            except OperationalError as e:
-                print(f"Database connection shut down: {str(e)}")
-                return
+            word_to_sql(engine, directory, file, size_limit)
 
         create_index(engine, directory)
 
 
-def create_index(engine: Engine, directory: str):
-        print("Indexing...")
-        for i in range(1, 6):
-            print(f'Attempt {i}:')
-            try:
-                engine.execute(
-                    f"CREATE INDEX {directory}_index ON {directory} (term, year)"
-                )
-                print(f"Index created for {directory}")
-                return
-            except ProgrammingError:
-                print("Index creation failed: Undefined table")
+@retry(10, 10)
+def word_to_sql(
+        engine: Engine,
+        directory: str,
+        file: Path,
+        size_limit: int,
+):
+    print(f'Reading {file.name}')
 
-        raise ConnectionError(f'Index creation for {directory} failed.')
+    df = pd.read_csv(file)
+
+    if df.empty:
+        print(f'{file.name} is empty, moving on')
+        return
+
+    df.sort_values(by='year', inplace=True)
+
+    df = df.head(size_limit)
+    df.index.rename('index', inplace=False)
+
+    df['term'] = [file.stem] * len(df.index)
+
+    if 'type' in df.columns:
+        df['lemma'] = df.type.isin(['lemma', 'both'])
+        df['regex'] = df.type.isin(['regex', 'both'])
+        df.drop(columns='type', inplace=True)
+
+    print(f'Saving {file.stem} to database')
+
+    df.to_sql(
+        directory,
+        con=engine,
+        if_exists='append',
+        index=False,
+        chunksize=100,
+    )
+
+
+@retry(20, 10)
+def create_index(engine: Engine, directory: str):
+    print("Indexing...")
+    engine.execute(
+        f"CREATE INDEX {directory}_index ON {directory} (term, year)"
+    )
+    print(f"Index created for {directory}")
 
 
 if __name__ == '__main__':
@@ -87,12 +115,12 @@ if __name__ == '__main__':
     with open('../../secrets') as fopen:
         database_url = fopen.read()
 
-        populate_database(
-            data_dir=data_dir,
-            database_url=database_url,
-            kwic_dirs=kwic_dirs,
-            size_limit=2000,
-        )
+    populate_database(
+        data_dir=data_dir,
+        database_url=database_url,
+        kwic_dirs=kwic_dirs,
+        size_limit=2000,
+    )
 
     # engine = create_engine(database_url)
     #
